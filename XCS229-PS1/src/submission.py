562b1a11a1a2ef4eaba13dcf723f0ff6acd67250
hhayden <hayden1298@hotmail.com>
2025-06-12 14:48:50 -0400

    finished hw 2-4

--- XCS229-PS1/src/submission.py
+++ XCS229-PS1/src/submission.py
@@ -37,6 +37,7 @@ class LinearModel(object):
     """
     pass
     # *** START CODE HERE ***
+    self.theta = np.linalg.solve(x.T.dot(x), x.T.dot(y))
     # *** END CODE HERE ***
 
   def predict(self, x):
@@ -51,6 +52,8 @@ class LinearModel(object):
     """
     pass
     # *** START CODE HERE ***
+    # return the features times the weights of the model
+    return x.dot(self.theta)
     # *** END CODE HERE ***
 
   @staticmethod
@@ -65,6 +68,11 @@ class LinearModel(object):
     """
     pass
     # *** START CODE HERE ***
+    p = np.ones((x.shape[0], k+1), dtype=np.float64)
+    for i in range(1, k+1):
+        p[:,i] = x[:,0] ** i
+    return p
+
     # *** END CODE HERE ***
 
   @staticmethod
@@ -78,32 +86,48 @@ class LinearModel(object):
       0) and polynomial (columns 1 to k+1) features of x with powers 0 to k
       (inclusive).
     """
-    pass
     # *** START CODE HERE ***
+    # pre allocate zeros
+    sin = np.zeros((x.shape[0], k+2), dtype=np.float64)
+    # first column is sin function
+    sin[:,0] = np.sin(x[:,0])
+    # second column is poly feature map of x
+    sin[:,1:] = LinearModel.create_poly(k,x)
+    return sin
     # *** END CODE HERE ***
 
 def run_exp(train_path, sine=False, ks=[1, 2, 3, 5, 10, 20], filename='plot.png'):
-  train_x,train_y=util.load_dataset(train_path,add_intercept=False)
-  plot_x = np.ones([1000, 1])
-  plot_x[:, 0] = np.linspace(-factor*np.pi, factor*np.pi, 1000)
-  plt.figure()
-  plt.scatter(train_x, train_y)
+    train_x,train_y=util.load_dataset(train_path,add_intercept=False)
+    plot_x = np.ones([1000, 1])
+    plot_x[:, 0] = np.linspace(-factor*np.pi, factor*np.pi, 1000)
+    plt.figure()
+    plt.scatter(train_x, train_y)
 
-  for k in ks:
-      '''
-      Our objective is to train models and perform predictions on plot_x data
-      '''
-      # *** START CODE HERE ***
-      # *** END CODE HERE ***
-      '''
-      Here plot_y are the predictions of the linear model on the plot_x data
-      '''
-      plt.ylim(-2, 2)
-      plt.plot(plot_x[:, 0], plot_y, label='k=%d' % k)
+    for k in ks:
+        # k is feature map -> 1, x, x^2, ... x^k
+        '''
+        Our objective is to train models and perform predictions on plot_x data
+        '''
+        # *** START CODE HERE ***
+        if sine:
+            train = LinearModel.create_sin(k, train_x)
+            plot = LinearModel.create_sin(k, plot_x)
+        else:
+            train = LinearModel.create_poly(k, train_x)
+            plot = LinearModel.create_poly(k, plot_x)
 
-  plt.legend()
-  plt.savefig(filename)
-  plt.clf()
+        model = LinearModel()
+        model.fit(train, train_y)
+        plot_y = model.predict(plot)
+        '''
+        Here plot_y are the predictions of the linear model on the plot_x data
+        '''
+        plt.ylim(-2, 2)
+        plt.plot(plot_x[:, 0], plot_y, label='k=%d' % k)
+
+    plt.legend()
+    plt.savefig(filename)
+    plt.clf()
 
 
 def main(train_path, small_path, eval_path):
--- XCS229-PS1/stability/stability.py
+++ XCS229-PS1/stability/stability.py
@@ -8,7 +8,7 @@ def calc_grad(X, Y, theta):
     """Compute the gradient of the loss with respect to theta."""
     count, _ = X.shape
 
-    probs = 1. / (1 + np.exp(-X.dot(theta)))
+    probs = 1.0 / (1 + np.exp(-X.dot(theta)))
     grad = (Y - probs).dot(X)
 
     return grad
@@ -20,28 +20,31 @@ def logistic_regression(X, Y):
     learning_rate = 0.1
 
     i = 0
-    while True:
+    while i < 500000:
         i += 1
         prev_theta = theta
         grad = calc_grad(X, Y, theta)
         theta = theta + learning_rate * grad
         if i % 10000 == 0:
-            print('Finished %d iterations' % i)
+            print("Finished %d iterations" % i)
         if np.linalg.norm(prev_theta - theta) < 1e-15:
-            print('Converged in %d iterations' % i)
+            print("Converged in %d iterations" % i)
             break
-    return
+
+    return theta
 
 
 def main():
-    print('==== Training model on data set A ====')
-    Xa, Ya = util.load_csv('ds1_a.csv', add_intercept=True)
-    logistic_regression(Xa, Ya)
+    print("==== Training model on data set A ====")
+    Xa, Ya = util.load_csv("ds1_a.csv", add_intercept=True)
+    theta = logistic_regression(Xa, Ya)
+    util.plot(Xa, Ya, theta, save_path="plot_a.png")
 
-    print('\n==== Training model on data set B ====')
-    Xb, Yb = util.load_csv('ds1_b.csv', add_intercept=True)
-    logistic_regression(Xb, Yb)
+    print("\n==== Training model on data set B ====")
+    Xb, Yb = util.load_csv("ds1_b.csv", add_intercept=True)
+    theta = logistic_regression(Xb, Yb)
+    util.plot(Xb, Yb, theta, save_path="plot_b.png")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
